5ac6869e0c372dbc4c73eaee7fa8b99a2a8543fd
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-06-26 15:54:37 +0530

    Added isLoggedIn middleware validator

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const userRouter = require('./routes/userRoute')
 const cartRouter = require('./routes/userRoute')
 const authRouter = require('./routes/authRoute')
 const productRouter = require('./routes/productRouter')
+const { isLoggedIn } = require('./validation/authValidator')
 
 const app = express()
 
 
-app.use(cookieParser()); // It is for accessing cookies on server
+app.use(cookieParser()); // It is for accessing cookies on server ( When User sends request it also contain token to read that token we use cookie parser)
 
 // If request is in JSON, text, urlencoded it correctly reads by Express Server
 app.use(bodyParser.json());
@@ -23,6 +24,14 @@ app.use('/users', userRouter) ; // Connects Router to the Server
 app.use('/carts', cartRouter) ;
 app.use('/auth', authRouter) ;
 app.use('/products', productRouter) ;
+
+
+// app.get('/ping', isLoggedIn, (req, res) => {
+//   // controller
+//   console.log(req.body);
+//   console.log(req.cookies);
+//   return res.json({message: "pong"});
+// })
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
