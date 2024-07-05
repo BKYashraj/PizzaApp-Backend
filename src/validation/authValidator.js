983074ea4ffc259a776c40d16731a1da4e7b8c3e
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-05 18:34:14 +0530

    All Done

--- src/validation/authValidator.js
+++ src/validation/authValidator.js
@@ -5,6 +5,8 @@ const { JWT_SECRET } = require("../config/serverConfig");
 async function isLoggedIn(req, res, next) {
   const token = req.cookies["authToken"]; // get the token from cookie
 
+  console.log(token);
+
   // if token is not provided, then user is not authenticated
   if (!token) {
     return res.status(401).json({
@@ -14,27 +16,51 @@ async function isLoggedIn(req, res, next) {
       message: "No Auth Token provided",
     });
   }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET); // verify the token with secret key
 
-  const decoded = jwt.verify(token, JWT_SECRET); // verify the token with secret key
+    console.log(decoded, decoded.exp, Date.now() / 1000);
 
-  if (!decoded) {
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        data: {},
+        error: "Not authenticated",
+        message: "Invalid Token provided",
+      });
+    }
+
+    // if reached here, then user is authenticated allow them to access the api
+
+    req.user = {
+      email: decoded.email,
+      id: decoded.id,
+      role: decoded.role,
+    };
+
+    next(); // move to next middleware
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "TokenExpiredError") {
+      res.cookie("authToken", "", {
+        httpOnly: true,
+        secure: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+      return res.status(200).json({
+        success: true,
+        message: "Log out successfull",
+        error: {},
+        data: {},
+      });
+    }
     return res.status(401).json({
       success: false,
       data: {},
-      error: "Not authenticated",
+      error: error,
       message: "Invalid Token provided",
     });
   }
-
-  // if reached here, then user is authenticated allow them to access the api
-
-  req.user = {
-    email: decoded.email,
-    id: decoded.id,
-    role: decoded.role,
-  };
-
-  next(); // move to next middleware
 }
 
 function isAdmin(req, res, next) {
@@ -43,7 +69,6 @@ function isAdmin(req, res, next) {
     console.log("User is an admin");
 
     next();
-
   } else {
     return res.status(401).json({
       success: false,
