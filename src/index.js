a324881baaa13ed4563ffaa9c01c97c93daac915
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-23 19:04:46 +0530

    Payment Integration Done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const authRouter = require('./routes/authRoute')
 const productRouter = require('./routes/productRouter')
 const { isLoggedIn } = require('./validation/authValidator')
 const orderRouter = require('./routes/orderRoute')
+const payment = require('./routes/payment');
 
 
 const app = express()
@@ -25,15 +26,17 @@ app.use(cors({
 app.use(cookieParser()); // It is for accessing cookies on server ( When User sends request it also contain token to read that token we use cookie parser)
 
 // If request is in JSON, text, urlencoded it correctly reads by Express Server
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
  
 app.use('/users', userRouter) ; // Connects Router to the Server
 app.use('/carts', cartRouter) ;
 app.use('/auth', authRouter) ;
 app.use('/products', productRouter) ;
 app.use('/orders', orderRouter) ;
+app.use('/api/payment', payment);
 
 app.get('/ping', (req, res) => {
   // controller
@@ -41,6 +44,8 @@ app.get('/ping', (req, res) => {
   console.log(req.cookies);
   return res.json({message: "pong"});
 });
+
+// RazerPay Integration
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
