4a755074d9ff016075e61a067ba4588c839daf5a
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-08-18 11:32:00 +0530

    Added changes

--- src/repositories/productRepository.js
+++ src/repositories/productRepository.js
@@ -1,46 +1,56 @@
 const Product = require("../schema/productSchema");
 
-async function createProduct(productDetails){
+async function createProduct(productDetails) {
   try {
     const response = await Product.create(productDetails);
     return response;
   } catch (error) {
-    console.error(error);
+    console.error("Error creating product:", error);
+    throw error; // Throw the error to handle it in the calling function
   }
 }
 
-async function findProductById(productId) { 
+async function findProductById(productId) {
   try {
     const response = await Product.findById(productId);
+    if (!response) {
+      throw new Error("Product not found");
+    }
     return response;
   } catch (error) {
-    console.log(error);
+    console.error("Error finding product by ID:", error);
+    throw error; // Throw the error to handle it in the calling function
   }
 }
 
-async function getAllProducts({ page = 1, limit = 10 }) {
+async function getAllProducts({ page = 1, limit = 5 }) {
   try {
     const products = await Product.find({})
                                   .skip((page - 1) * limit)
                                   .limit(limit);
     return products;
   } catch (error) {
-    console.log(error);
+    console.error("Error getting all products:", error);
+    throw error; // Throw the error to handle it in the calling function
   }
 }
 
-async function deleteProductById(productId) { 
+async function deleteProductById(productId) {
   try {
     const response = await Product.findByIdAndDelete(productId);
+    if (!response) {
+      throw new Error("Product not found or already deleted");
+    }
     return response;
   } catch (error) {
-    console.log(error);
+    console.error("Error deleting product by ID:", error);
+    throw error; // Throw the error to handle it in the calling function
   }
 }
 
 module.exports = {
   createProduct,
   findProductById,
+  getAllProducts,
   deleteProductById,
-  getAllProducts
-}
+};
